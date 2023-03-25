b360b20b3c5b546e54465075246c529d83a7735e
emmanuelguty <manuel15901@gmail.com>
2023-03-25 17:16:45 -0600

    List Tasks Frontend

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,4 +1,10 @@
 import axios from "axios"
 
+//Para cargar tareas
+export const getTaskRequest = async () => 
+   await axios.get('http://localhost:3002/tasks')
+
+
+// Para crear tareas
 export const createTaskRequest = async (task) => 
     await axios.post('http://localhost:3002/tasks', task);
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react"
+
+import TaskCard from "../components/TaskCard"
+import { getTaskRequest } from "../api/task.api"
+
 const TasksPage = () => {
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    async function loadTask() {
+      const response = await getTaskRequest()
+      setTasks(response.data)
+    }
+    loadTask()
+
+  }, [])
+
   return (
-    <div>TasksPage</div>
+    <div>
+      <h1>Tasks</h1>
+      {
+        tasks.map(task => (
+          <TaskCard task={task} key={task.id}/>
+        ))
+      }
+    </div>
   )
 }
 
